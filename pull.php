480eae9978f25aba88a308d902a6d202949d1729
lafor038 <lafor038@umn.edu>
2022-02-02 12:48:22 -0600

    updated pull

--- pull.php
+++ pull.php
@@ -1,6 +1,11 @@
 <?php
-shell_exec('chmod -R o+rw .git');
+shell_exec('chmod -R 755 .git');
+shell_exec('chmod o+rw .git');
+echo shell_exec('ls -ld .git');
+echo '<br>';
 shell_exec('cd .git && chmod o+rw index');
+echo shell_exec('cd .git && ls -l index');
+echo '<br>';
 $output = shell_exec('cd .git && ls -la && cd ..');
 echo "<pre>$output</pre>";
 echo shell_exec('git pull 2>&1');
