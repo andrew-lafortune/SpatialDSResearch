59589b0bb68fbe903a37845517399714e81d7c75
andrew-lafortune <andrewlafortune@gmail.com>
2022-02-02 12:54:58 -0600

    updated pull.php

--- pull.php
+++ pull.php
@@ -1,9 +1,10 @@
 <?php
 echo shell_exec('chmod -R o+rw .git');
-echo shell_exec('git pull 2>&1');
+echo shell_exec('git reset --hard HEAD && git pull 2>&1');
 echo '<br>';
 `chmod o+rw index.html`;
 echo shell_exec('whoami');
 $output = shell_exec('ls -la');
 echo "<pre>$output</pre>";
+shell_exec('touch webhook.php')
 ?>
